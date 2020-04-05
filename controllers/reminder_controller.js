f88c3d666263731117c4c4ada9bbff3ed5f190ba
ryanl <rleung1004@gmail.com>
2020-04-05 14:45:45 -0700

    merged api-integration branch

--- controllers/reminder_controller.js
+++ controllers/reminder_controller.js
@@ -1,24 +1,30 @@
+const fetch = require("node-fetch");
 let Database = require("../database");
 let currentDate = require("../utility/handyFunctions");
+let coordinates = { lat: undefined, lon: undefined };
+let weatherData;
 
 let remindersController = {
   list: (req, res) => {
-    res.render('reminder/index', { reminders: Database.cindy.reminders, mode: Database.cindy.mode })
+    res.render("reminder/index", {
+      reminders: Database.cindy.reminders,
+      mode: Database.cindy.mode,
+    });
   },
 
   new: (req, res) => {
-    res.render('reminder/create')
+    res.render("reminder/create");
   },
 
   listOne: (req, res) => {
     let reminderToFind = req.params.id;
-    let searchResult = Database.cindy.reminders.find(function(reminder) {
+    let searchResult = Database.cindy.reminders.find(function (reminder) {
       return reminder.id == reminderToFind; // good test question for students what happens if I put ===
-    })
+    });
     if (searchResult != undefined) {
-      res.render('reminder/single-reminder', { reminderItem: searchResult })
+      res.render("reminder/single-reminder", { reminderItem: searchResult });
     } else {
-      res.render('reminder/index', { reminders: Database.cindy.reminders })
+      res.render("reminder/index", { reminders: Database.cindy.reminders });
     }
   },
 
@@ -27,65 +33,74 @@ let remindersController = {
     let current = currentDate();
 
     let reminder = {
-      id: Database.cindy.reminders.length+1,
+      id: Database.cindy.reminders.length + 1,
       title: req.body.title,
       description: req.body.description,
       remindDate: req.body.remindDate,
       currentDate: req.body.remindDate == current,
-      completed: false
-    }
+      completed: false,
+    };
     Database.cindy.reminders.push(reminder);
-    res.redirect('/reminder');
+    res.redirect("/reminder");
   },
 
   edit: (req, res) => {
     let reminderToFind = req.params.id;
-    let searchResult = Database.cindy.reminders.find(function(reminder) {
+    let searchResult = Database.cindy.reminders.find(function (reminder) {
       return reminder.id == reminderToFind; // Why do you think I chose NOT to use === here?
-    })
-    res.render('reminder/edit', { reminderItem: searchResult })
-    
+    });
+    res.render("reminder/edit", { reminderItem: searchResult });
   },
 
   update: (req, res) => {
     let reminderToFind = req.params.id;
-    let searchResult = Database.cindy.reminders.find(function(reminder) {
-      if(reminder.id == reminderToFind) {
-        reminder.title = req.body.title,
-        reminder.description = req.body.description,
-        reminder.remindDate = req.body.remindDate,
-        // Why do you think I had to do req.body.completed == "true" below?
-        reminder.completed = req.body.completed == "true" 
+    let searchResult = Database.cindy.reminders.find(function (reminder) {
+      if (reminder.id == reminderToFind) {
+        (reminder.title = req.body.title),
+          (reminder.description = req.body.description),
+          (reminder.remindDate = req.body.remindDate),
+          // Why do you think I had to do req.body.completed == "true" below?
+          (reminder.completed = req.body.completed == "true");
       }
     });
-    res.redirect('/reminder/' + reminderToFind)
+    res.redirect("/reminder/" + reminderToFind);
   },
 
   delete: (req, res) => {
     let reminderToFind = req.params.id;
-    let reminderIndex = Database.cindy.reminders.findIndex(function(reminder) {
-      return reminder.id == reminderToFind; 
-    })
+    let reminderIndex = Database.cindy.reminders.findIndex(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
     Database.cindy.reminders.splice(reminderIndex, 1);
-    res.redirect('/reminder');
+    res.redirect("/reminder");
   },
 
   complete: (req, res) => {
     let reminderToFind = req.params.id;
-    let searchResult = Database.cindy.reminders.find(function(reminder) {
+    let searchResult = Database.cindy.reminders.find(function (reminder) {
       if (reminder.id == reminderToFind) {
         if (reminder.completed == true) {
           reminder.completed = false;
-        }
-        else {
+        } else {
           reminder.completed = true;
-        };
+        }
         res.redirect("/reminder");
       }
-    })
-  }
+    });
+  },
 
-/* 
+  getWeatherData: async (req, res) => {
+    const coordinates = req.params.coordinates.split(",");
+    const lat = coordinates[0];
+    const lon = coordinates[1];
+    let api_url = `https://api.darksky.net/forecast/adaec7279bd9fe4d04fac4d6ecd1052b/${lat},${lon}`;
+    let fetch_response = await fetch(api_url);
+    weatherData = await fetch_response.json();
+    res.json(weatherData);
+    let date = req.body.remindDate;
+  },
+
+  /* 
   // does not work... ask Armaan
   deleteSelected: (req, res) => {
     let check_box_array = document.getElementsByClassName("custom-control-input");
@@ -96,9 +111,6 @@ let remindersController = {
     }
     res.redirect("/reminder")
   } */
+};
 
-
-}
-
-
-module.exports = remindersController
+module.exports = remindersController;
--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-const express = require("express")
-const app = express()
-const ejsLayouts = require("express-ejs-layouts")
+const express = require("express");
+const app = express();
+const ejsLayouts = require("express-ejs-layouts");
 const reminderController = require("./controllers/reminder_controller");
 
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({ extended: false }))
+app.use(ejsLayouts);
 
-app.use(ejsLayouts)
-
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 // Routes start here
 
-app.get("/reminder", reminderController.list)
+app.get("/reminder", reminderController.list);
 
-app.get("/reminder/new", reminderController.new)
+app.get("/reminder/new", reminderController.new);
 
-app.get("/reminder/:id", reminderController.listOne)
+app.get("/reminder/:id", reminderController.listOne);
 
-app.get("/reminder/:id/edit", reminderController.edit)
+app.get("/reminder/:id/edit", reminderController.edit);
 
-app.post("/reminder/", reminderController.create)
+app.post("/reminder/", reminderController.create);
 
-app.post("/reminder/update/:id", reminderController.update) // suggestion for class: look into put and post
+app.post("/reminder/update/:id", reminderController.update); // suggestion for class: look into put and post
 
-app.post("/reminder/delete/:id", reminderController.delete)
+app.post("/reminder/delete/:id", reminderController.delete);
 
-app.post("/reminder/complete/:id", reminderController.complete)
+app.post("/reminder/complete/:id", reminderController.complete);
+
+app.get("/weather/:coordinates", reminderController.getWeatherData);
 
 /* /// does not work... ask Armaan
 app.post("/reminder/deleteSelected/:id", reminderController.deleteSelected) */
 
-app.listen(3000, function(){
-  console.log("Server running. Visit: localhost:3000/reminder in your browser 🚀");
-})
-
+app.listen(3000, function () {
+  console.log(
+    "Server running. Visit: localhost:3000/reminder in your browser 🚀"
+  );
+});
